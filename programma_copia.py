bd80c99c134e8ba95dbd8105a3b92fd4ecacc775
Matteo Fonsetti <56176175+matteofonsetti@users.noreply.github.com>
2019-12-13 18:40:36 +0100

    Add files via upload

--- programma_copia.py
+++ programma_copia.py
@@ -20,50 +20,78 @@ def geocutoff_cumani2(l):
     l_rad = l*np.pi/180
     return (11.9*np.cos(l_rad)**4)               # equazione alleggerita ((num/den) = 1)
 
-def proton_flux(T):
-    C = 0.4544                                   # fattore di normalizzazione
-    gamma = -2.849                               # costante
-    dlt_gamma = 0.133                            # costante
-    s = 0.024                                    # smoothness of transition (?)
-    R0 = 336                                     # GV
-    A = 1                                        # numero di massa protone
-    Z = 1                                        # numero atomico protone
-    T0 = 938.25*10**(-3)                         # energia a riposo protone (GeV)
-    M0 = 1.6726219*10**(-27)                     # massa protone
-    arg = (T**2 + 2*T*T0)**(0.5)
-    arg_out = (T**2 + 2*T*M0)**(0.5)
+def g10(g):
+    g10_values=[31543, 31464, 31354, 31212, 31060, 30926, 30805, 30715, 30654, 30594, 30554, 30500, 30421, 30334, 30220, 30100, 29992, 29873, 29775, 29692, 29619.4, 29554.63, 29496.57, 29442.0]
+    return (g10_values)
+
+def g10_var(g):
+    g10_var=[79, 110, 142, 152, 134, 121, 90, 61, 60, 40, 54, 54, 79, 87, 114, 120, 108, 119, 98, 83, 72.6, 64.77, 58.06, 54.57]
+    return (g10_var)
     
-    R = (A/Z)*arg
-    R_out = arg_out = (T**2 + 2*T*M0)**(0.5)
-    F = ((C*(R/45)**gamma)*(1+(R/R0)**(dlt_gamma/s))**s)*R_out**2.7
+#def proton_flux(R):
+#    C = 0.4544                                   # fattore di normalizzazione
+#    gamma = -2.849                               # costante
+#    dlt_gamma = 0.133                            # costante
+#    s = 0.024                                    # smoothness of transition (?)
+#    R0 = 336                                     # GV
+#    A = 1                                        # numero di massa protone
+#    Z = 1                                        # numero atomico protone
+    #T0 = 938.25*10**(-3)                         # energia a riposo protone (GeV)
+    #M0 = 1.6726219*10**(-27)                     # massa protone
+    #arg = (T**2 + 2*T*T0)**(0.5)
+    #arg_out = (T**2 + 2*T*M0)**(0.5)
     
-    return (F)
+    #R = (A/Z)*arg
+    #R_out = arg_out = (T**2 + 2*T*M0)**(0.5)
+    #F1 = (C*(R/45)**gamma)
+    #F2 = (1+(R/R0)**(dlt_gamma/s))**s
+    #F = ((C*(R/45)**gamma)*(1+(R/R0)**(dlt_gamma/s))**s)*R**2.7
+    #f = (F1*F2)*R**(-2.7)
+    
+#    return (f)
 
 
    
 h = np.linspace(400,600,20)                       # ascisse altitudine
 l = np.linspace(0,50,20)                          # ascisse latitudine
-T = np.logspace(2,3,base=10.0)                    # ascisse energia cinetica (GeV)
-#T = np.linspace(1,100000,10)
+g = np.linspace(1900,2015,24)                     # ascisse anno
+#R = np.logspace(2,3,base=10.0)                    # ascisse energia cinetica (GeV)
+#R = np.linspace(1,100000,10)
 
 fig, ax1 = plt.subplots()
 ax1.set_xlabel('Altitude / km', color = 'firebrick')
 ax1.set_ylabel('Rcutoff / GV')
-line1, = ax1.plot(h, geocutoff_cumani(h), '--', color = 'firebrick', label = 'Geomagnetic Latitude = 0 degrees')
+line1, = ax1.plot(h, geocutoff_cumani(h), '--', color = 'firebrick', label = 'Geomagnetic Latitude = 0$^{\circ}$')
 ax1.tick_params(axis='x', labelcolor = 'firebrick')
 
 ax2 = ax1.twiny()
-
 color_blue = 'tab:blue'
-ax2.set_xlabel('Geomagnetic Latitude / degrees', color = color_blue)
+ax2.set_xlabel('Geomagnetic Latitude / $^{\circ}$', color = color_blue)
 line2, = ax2.plot(l, geocutoff_cumani2(l), color = color_blue, label = 'Altitude = 550 km')
 ax2.tick_params(axis='x', labelcolor = color_blue)
 ax1.grid(axis = 'y')
-ax1.legend((line1, line2), ('Geomagnetic Latitude = 0 degrees', 'Altitude = 550 km'))
+ax1.legend((line1, line2), ('Geomagnetic Latitude = 0$^{\circ}$', 'Altitude = 550 km'))
 fig.tight_layout()                                # facoltativo: allunga un pelo il grafico
 plt.show()
+fig.savefig('tesi\Cut-off_latitude_altitude.png', bbox_inches='tight')
 
-plt.plot (T, proton_flux(T))
+
+figg, ax1g = plt.subplots()
+ax1g.set_xlabel('Year')
+ax1g.set_ylabel('g$_{1}^{0}$ value')
+line1, = ax1g.plot(g, g10(g), color = 'blue')
+ax1g.tick_params(axis='x')
+
+ax2g = ax1g.twinx()
+line2, = ax2g.plot(g, g10_var(g), color = 'red')
+ax2g.set_ylabel('g$_{1}^{0}$ variation')
+figg.tight_layout()
+ax1g.grid(axis = 'x')
+ax1g.grid(axis = 'y')
+plt.show()
+figg.savefig('tesi\g10 values and variations')
+
+#plt.plot (R, proton_flux(R))
 
 """    
 #def geocutoff_l(l):                      #funzione x geocutoff a h costante
